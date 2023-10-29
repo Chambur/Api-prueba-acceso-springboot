59e47ed68643cb2135540041c253ca58cea6955f
Chambur <samu.lp22@gmail.com>
2023-10-29 17:05:40 +0000

    Final commit.

--- src/main/java/com/example/prueba_acceso/prueba_acceso/PruebaAccesoApplication.java
+++ src/main/java/com/example/prueba_acceso/prueba_acceso/PruebaAccesoApplication.java
@@ -11,6 +11,18 @@ public class PruebaAccesoApplication {
 		SpringApplication.run(PruebaAccesoApplication.class, args);
 	}
 
-
+	public void exercice2 (){
+		for (int i = 1; i <= 100; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				System.out.println("VINCLE");
+			} else if (i % 3 == 0) {
+				System.out.println("VIN");
+			} else if (i % 5 == 0) {
+				System.out.println("CLE");
+			} else {
+				System.out.println(i);
+			}
+		}
+	}
 
 }
